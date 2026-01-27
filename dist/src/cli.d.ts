ad525e5d03941d0967bd769eeb03401065326068
Wolfe-Jam <wolfejam2020@gmail.com>
2026-01-27 05:07:49 -0500

    chore: Rebuild dist with faf-cli dependency

---
+++ dist/src/cli.d.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/**
+ * faf-clawdbot CLI
+ * Portable souls for ClawdBot
+ *
+ * IANA: application/vnd.faf+yaml
+ * https://faf.one
+ */
+export {};
+//# sourceMappingURL=cli.d.ts.map
